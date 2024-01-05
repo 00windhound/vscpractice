8bb7d7e60d3425cc043e1038eb33f4ef0ab79338
crystalyn <00crystalynthompson@gmail.com>
2024-01-05 01:31:10 -0800

    4 players

--- java.js
+++ java.js
@@ -2,6 +2,7 @@ document.getElementById("create").onclick = function(){
    const p1 = document.getElementById("p1");
    const p2 = document.getElementById("p2");
    const p3 = document.getElementById("p3");
+   const p4 = document.getElementById("p4")
    
    
 if(document.getElementById("a").checked){
@@ -16,7 +17,10 @@ if(p1.checked){
         document.getElementById("c").innerHTML="player 2";
     }
     else if(p3.checked){
-        document.getElementById("c").innerHTML="player 3"
+        document.getElementById("c").innerHTML="player 3";
+    }
+    else if(p4.checked){
+        document.getElementById("c").innerHTML="player 4";
     }
     else{document.getElementById("c").innerHTML="none chosen"}
 
