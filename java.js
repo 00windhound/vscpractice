6edba3d1e6468c205d824b5b1a3c2bc2bc165e51
crystalyn <00crystalynthompson@gmail.com>
2024-01-04 23:33:55 -0800

    js

--- java.js
+++ java.js
@@ -1,8 +1,23 @@
 document.getElementById("create").onclick = function(){
-    if(document.getElementById("a").checked){
+   const p1 = document.getElementById("p1");
+   const p2 = document.getElementById("p2");
+   const p3 = document.getElementById("p3");
+   
+   
+if(document.getElementById("a").checked){
         document.getElementById("b").innerHTML="true";
-
-    }
+ }
     else{document.getElementById("b").innerHTML="false";
 }
+if(p1.checked){
+    document.getElementById("c").innerHTML="player 1";
+}
+    else if(p2.checked){
+        document.getElementById("c").innerHTML="player 2";
+    }
+    else if(p3.checked){
+        document.getElementById("c").innerHTML="player 3"
+    }
+    else{document.getElementById("c").innerHTML="none chosen"}
+
 }
