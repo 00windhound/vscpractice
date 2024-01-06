79f1c5a8cf1008f1734a5095a9fde14d6441120b
crystalyn <00crystalynthompson@gmail.com>
2024-01-06 01:18:28 -0700

    i have my work cut out for me

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ document.getElementById("submit").onclick = function(){
    const p2 = document.getElementById("p2");
    const p3 = document.getElementById("p3");
    const p4 = document.getElementById("p4")
-   const p;
+   const p = p;
    
 if(document.getElementById("a").checked){
         document.getElementById("b").innerHTML="true";
@@ -12,18 +12,19 @@ if(document.getElementById("a").checked){
 }
 if(p1.checked){
    // document.getElementById("c").innerHTML="player 1";
-}   p= p1;
+   p= p1;
+}
     else if(p2.checked){
        // document.getElementById("d").innerHTML="player 2";
-        p=p2
+        p=p2;
     }
     else if(p3.checked){
         //document.getElementById("e").innerHTML="player 3";
-        p=p3
+        p=p3;
     }
     else if(p4.checked){
         //document.getElementById("f").innerHTML="player 4";
-        p=p4
+        p=p4;
     }
     else{document.getElementById("c").innerHTML="none chosen"}
 
